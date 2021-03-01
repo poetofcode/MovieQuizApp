bd1de5fa1bba0958a43f44b85659be65c305113e
poetofcode <poetinthecode@gmail.com>
2021-03-01 23:56:59 +0300

    Implementing game logic

--- app/src/main/java/ru/poetofcode/whatahorror/GameLogic.kt
+++ app/src/main/java/ru/poetofcode/whatahorror/GameLogic.kt
@@ -1,8 +1,8 @@
 package ru.poetofcode.whatahorror
 
-class GameLogic(val view: IView) {
+class GameLogic(private val view: IView) {
 
-    val lastQuestions: ArrayList<Question> = ArrayList()
+    private val lastQuestions: ArrayList<Question> = ArrayList()
 
     init {
         resetGame()
@@ -13,7 +13,7 @@ class GameLogic(val view: IView) {
         view.showQuestion(q.description, q.imageUrls[0], q.variants)
     }
 
-    fun getCurrQuestion(): Question {
+    private fun getCurrQuestion(): Question {
         if (lastQuestions.size > 0 && lastQuestions.last().result.isAnswered()) {
             return lastQuestions.last()
         }
@@ -24,7 +24,10 @@ class GameLogic(val view: IView) {
 
     private fun createQuestion(): Question {
 
-        // TODO implement logic of creation new question
+        // TODO implement logic of creation new question:
+        //  ..
+        //  1. Create new interface MovieProvider
+        //  2. Define methods for getting data for building question
 
         return Question(
             description = "Test movie name",
@@ -34,11 +37,17 @@ class GameLogic(val view: IView) {
         )
     }
 
-    fun reply(id: Int): Boolean {
+    fun reply(selectedVariant: Int) {
+        val q = getCurrQuestion()
 
-        // TODO implement it
-
-        return false
+        if (selectedVariant == q.indexOfRightVariant) {
+            view.markVariantAsRight(selectedVariant)
+            view.showResult("Вы угадали!")
+        } else {
+            view.markVariantAsWrong(selectedVariant)
+            view.markVariantAsRight(q.indexOfRightVariant)
+            view.showResult("Ответ неверный :(")
+        }
     }
 
     fun resetGame() {
--- app/src/main/java/ru/poetofcode/whatahorror/IView.kt
+++ app/src/main/java/ru/poetofcode/whatahorror/IView.kt
@@ -4,4 +4,10 @@ interface IView {
 
     fun showQuestion(description: String, imageUrl: String, variants: List<String>)
 
+    fun markVariantAsRight(variantIndex: Int)
+
+    fun markVariantAsWrong(variantIndex: Int)
+
+    fun showResult(resultText: String)
+
 }
