11f60c679cbda8d81ead226f855f90e75154e359
poetofcode <poetinthecode@gmail.com>
2021-03-03 17:56:40 +0300

    Passed test for RandomHelper

--- app/src/main/java/ru/poetofcode/whatahorror/helper/RandomHelper.kt
+++ app/src/main/java/ru/poetofcode/whatahorror/helper/RandomHelper.kt
@@ -1,9 +1,12 @@
 package ru.poetofcode.whatahorror.helper
 
+import kotlin.random.Random
+
 open class RandomHelper {
 
     open fun fromRange(range: IntRange, exceptList: List<Int>? = null): Int {
-        return 0
+        val filteredList = range.filter { value ->  exceptList?.find { value == it } == null }
+        return filteredList[Random.nextInt(filteredList.size)]
     }
 
 }
